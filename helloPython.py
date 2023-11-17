97c7d5dd955dcbb4ed5a9e299ac6dddcbf98f3bb
dewdaddy35 <eric.wells41@gmail.com>
2023-11-16 20:54:32 -0800

    first commit

---
+++ helloPython.py
@@ -0,0 +1,33 @@
+#let name = "Eric"; javascript
+name = "Eric"
+total = 12.50
+found = False
+age = 54
+print(name)
+print(total)
+print(found)
+
+#if statement javascript
+
+if age < 50:
+    print("You are below 50 years old")
+elif age > 50:
+    print("You are Older then 50 years old")
+    
+print("Well looks like i am outside the if statement")
+
+#functions
+#function(){logic};
+def say_hello():
+    print("hello")
+
+def say_goodbye(name):
+    print("Goodbye "+name)
+
+
+#call the function
+say_hello()
+say_goodbye(name)
+    
+    
+   
